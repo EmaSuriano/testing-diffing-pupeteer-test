fd570af869e49311ec999172f969d8b8f2e5984a
EmaSuriano <emanuel.suriano@gmail.com>
2018-10-18 16:09:48 +0200

    Adding puppeteer config, but not working :/

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import puppeteer from 'puppeteer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.setTimeout(30000)
+
+describe('App', () => {
+  let browser;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({ headless: false });
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  it('should display "google" text on page', async () => {
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    const bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    console.log(bodyHTML);
+    expect(bodyHTML).toMatch('google');
+  });
 });
